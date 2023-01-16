4b35f26bc0c3565d8adc2eda0dc89ccfa0b0c9ab
Windula Dissanayake <charithwinwindula@gmail.com>
2023-01-17 05:16:13 +0530

    commit #2

---
+++ Variables/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        char var1 = 'h';
+        String var2 = "My name is Windula";
+
+        System.out.println(var1);
+        System.out.println(var2);
+    }
+}

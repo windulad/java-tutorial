121cfad7fd993b2e01fce0c345674f28bf7181a1
Windula Dissanayake <charithwinwindula@gmail.com>
2023-01-18 02:30:40 +0530

    commit new java projects

---
+++ Mathoperations/src/math.java
@@ -0,0 +1,7 @@
+public class math {
+    public static void main(String[] args){
+        double var1 = 5;
+        double var2 = 10;
+        System.out.println(var1+var2);
+    }
+}
---
+++ Mathoperations/src/string.java
@@ -0,0 +1,10 @@
+public class string {
+    public static void main(String[] args){
+        int var1 = 15;
+        String var2 = "hello I am";
+        String var3 = "years old";
+        System.out.println(var2);
+        System.out.println(var1);
+        System.out.println(var3);
+    }
+}

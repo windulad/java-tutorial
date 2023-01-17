159993f707a643b976490d2a2a197a39960d5d28
Windula Dissanayake <charithwinwindula@gmail.com>
2023-01-18 02:56:07 +0530

    commit scanner method project

---
+++ ScannerMethod/src/scan.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class scan {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String txt = scan.next();
+        System.out.println(txt);
+    }
+}
